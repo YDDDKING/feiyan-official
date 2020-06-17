da8fdbde9975521e2a18421ab4c7a540d67515f7
YDDDKING <57852907+YDDDKING@users.noreply.github.com>
2020-06-17 17:44:00 +0800

    http + s

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let http = axios.create()
 // http.defaults.baseURL = 'http://10.10.10.19:9900'
-http.defaults.baseURL = 'http://api.fengtengge.com/'
+http.defaults.baseURL = 'https://api.fengtengge.com/'
 // http.defaults.baseURL = 'https://www.feiyanmath.com'
 
 export default http
